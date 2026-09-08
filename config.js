1bc1c22d942c5a18db2bfaf601cc0b5ff2509b90
adamjr040608-svg <adamjr040608@gmail.com>
2026-09-08 10:33:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   CHANNEL_LINK: "https://t.me/cuannreward",
   ADMIN_USERNAME: "icaa181",
 
-  START_BALANCE: 30000,
+  START_BALANCE: 150000,
   REFERRAL_BONUS: 35000,
   MIN_WITHDRAW: 150000
 };
