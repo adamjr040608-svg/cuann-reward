933909eb250fc8454fed75886726f357a434a4aa
adamjr040608-svg <adamjr040608@gmail.com>
2026-09-08 19:11:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ module.exports = {
   CHANNEL: "@cuannreward",
   CHANNEL_LINK: "https://t.me/cuannreward",
   ADMIN_USERNAME: "cuanwordd",
+  ADMIN_CHAT_ID: "7313589632",
 
   START_BALANCE: 30000,
   REFERRAL_BONUS: 35000,
